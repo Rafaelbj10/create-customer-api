2959f1e158be509cfafa4d52a4372c28449c4a4b
rafaelbo <rbatistaa321@gmail.com>
2026-06-12 23:32:00 -0300

    atualizando branch main

--- src/main/java/com/create/customer/service/impl/SnsServiceImpl.java
+++ src/main/java/com/create/customer/service/impl/SnsServiceImpl.java
@@ -2,6 +2,8 @@ package com.create.customer.service.impl;
 
 import com.create.customer.events.CustomerCreatedEvent;
 import com.create.customer.service.SnsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.SnsException;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -18,6 +24,7 @@ public class SnsServiceImpl implements SnsService {
 
     private final SnsClient snsClient;
     private final Tracer tracer;
+    private final ObjectMapper objectMapper;
 
     @Value("${aws.sns.customer-created-topic-arn}")
     private String topicArn;
@@ -25,7 +32,9 @@ public class SnsServiceImpl implements SnsService {
     @Override
     public void sendCustomerCreatedEvent(CustomerCreatedEvent event) {
 
-        Span span = tracer.nextSpan().name("sns.publish.customer-created").start();
+        Span span = tracer.nextSpan()
+                .name("sns.publish.customer-created")
+                .start();
 
         try (Tracer.SpanInScope ws = tracer.withSpan(span)) {
 
@@ -33,34 +42,39 @@ public class SnsServiceImpl implements SnsService {
             span.tag("messaging.system", "sns");
             span.tag("messaging.destination", topicArn);
 
-            String messageBody = """
-                    {
-                        "customerId": "%s",
-                        "cpf": "%s",
-                        "monthlyIncome": %s,
-                        "timestamp": "%s"
-                    }
-                    """.formatted(
-                    event.getCustomerId(),
-                    event.getCpf(),
-                    event.getMonthlyIncome(),
-                    event.getTimestamp()
-            );
+            String messageBody = objectMapper.writeValueAsString(event);
 
             PublishRequest request = PublishRequest.builder()
                     .topicArn(topicArn)
                     .message(messageBody)
+                    .messageAttributes(Map.of(
+                            "eventType", MessageAttributeValue.builder()
+                                    .dataType("String")
+                                    .stringValue("CustomerCreated")
+                                    .build()
+                    ))
                     .build();
 
             snsClient.publish(request);
 
-            log.info("CustomerCreatedEvent published to SNS topic customerId={}",
-                    event.getCustomerId());
+            log.info(
+                    "Published CustomerCreatedEvent customerId={} topicArn={}",
+                    event.getCustomerId(),
+                    topicArn
+            );
 
-        } catch (Exception e) {
-            span.tag("error", e.getMessage());
-            log.error("Failed to publish CustomerCreatedEvent customerId={} error={}",
-                    event.getCustomerId(), e.getMessage());
+        } catch (SnsException | JsonProcessingException e) {
+
+            span.error(e);
+
+            log.error(
+                    "Failed to publish CustomerCreatedEvent customerId={}",
+                    event.getCustomerId(),
+                    e
+            );
+
+            throw new RuntimeException("Error publishing SNS event", e);
+
         } finally {
             span.end();
         }
